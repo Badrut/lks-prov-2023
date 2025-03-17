e2cf709dfff2322fbc09cc2432498808f97e152a
Badrut <ayiiwyz@gmail.com>
2025-03-17 10:21:48 +0700

    #4

--- app/Http/Controllers/JobApplySocietieController.php
+++ app/Http/Controllers/JobApplySocietieController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\JobApplyPosition;
 use App\Models\JobApplySocietie;
 use App\Models\Societie;
+use App\Models\Validation;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -13,33 +14,47 @@ class JobApplySocietieController extends Controller
     public function store(Request $request)
     {
         try {
-            $request->validate([
+            $credentials = $request->validate([
                 'vacancy_id' => 'required',
                 'positions' => 'required',
-                'note' => 'required'
+                'notes' => 'required'
             ]);
 
             $token = Societie::where('login_tokens' , $request->token)->first();
+            $valid = JobApplySocietie::where('society_id' , $token->id)->get();
+            $validation = Validation::where('society_id' , $token->id)->get();
+            if($validation->status !== 'pending')
+            {
+            if(is_null($valid))
+            {
+                $societie = JobApplySocietie::create([
+                    'job_vacancy_id' => $request->vacancy_id,
+                    'society_id' => $token->id,
+                    'notes' => $request->notes,
+                    'date' => now()
+                ]);
 
-            $societie = JobApplySocietie::create([
-                'job_vacancy_id' => $request->vacancy_id,
-                'society_id' => $token->id,
-                'notes' => $request->notes
-            ]);
+                $job = JobApplyPosition::create([
+                    'job_vacancy_id' => $request->vacancy_id,
+                    'position_id' => $request->positions,
+                    'society_id' => $token->id,
+                    'job_apply_societies_id' => $societie->id,
+                    'date' => now()
+                ]);
 
-            $job = JobApplyPosition::create([
-                'job_vacancy_id' => $request->vacancy_id,
-                'position_id' => $request->positions,
-                'job_apply_societies_id' => $societie->id,
-                'data' => now()
-            ]);
-
-            return response()->json(['data' => $societie , "datrav2" => $job]);
+                return response()->json(['message' => 'Applying for job successful']);
+            }
+            else {
+                return response()->json(['message' => 'Application for a job can only be once']);
+            }
+        } else {
+            return response()->json(['message' => 'Your data validator must be accepted by validator before']);
+        }
         }
 
         catch(ValidationException $e)
         {
-            return;
+            return response()->json(['message' => $e->errors()]);
         }
     }
 }
